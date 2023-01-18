3417932e2b8cea0195dc0629770f4540818d6920
Francisco Zúñiga <francisco.zunigap@usm.cl>
2023-01-18 17:14:01 -0300

    update

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const mongoose = require("mongoose");
+const {v4: uuid} = require("uuid");
+
 
 const app = express()
 const url = "mongodb://127.0.0.1:27017/form" 
@@ -11,6 +13,7 @@ mongoose.connect(url,{useNewUrlParser: true})
 .catch((e)=>console.log("error de conexión es: "+ e));
 
 const userSchema = mongoose.Schema({
+    id: String,
     user: String,
     password: String
 
@@ -27,12 +30,59 @@ const verify = async (req)=>{
     return(verificador)
 }
 
+const mostrar = async ()=>{
+    
+    const usuarios = await userModel.find()
+    
+    return (usuarios)
+
+}
+
+//actualizar
+
+const actualizar = async(id, new_data)=>{
+
+    const user_act = await userModel.updateOne({
+        
+        id: id
+    
+        }, 
+        {
+        
+        $set: new_data
+
+        })
+
+    return true
+
+}
+
+//delete
+
+const eliminar = async(id_delete)=>{
+    const for_delete = await userModel.deleteOne({id: id_delete})
+    
+    return true
+
+}
 
 
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
+
+//VIEW 
+
+app.get("/admin",(req, res)=>{
+    var users = {};
+
+    mostrar().then((val)=>{
+        res.send(val)
+    })
+    
+    
+});
 
 
 
@@ -84,6 +134,8 @@ app.post("/register", [
         return res.json({code: 300}); ////este usuario ya existe
 
     }else{
+        //usuario a crear está en req.body (no posee id)
+        req.body.id = uuid(); // se le asigna id
         userModel.create(req.body); //crea usuario
         return res.json({code: 301})
 
@@ -93,8 +145,24 @@ app.post("/register", [
 )
 })
 
+/*/
+// Update
 
+app.post("/update", [],(req, res)=>{
+    actualizar(req.body.id, req.body).then((val)=>{
+        res.send("OK ACTUALIZADO")
 
+    })
+
+    
+    
+}
+)
+
+/*/
+
+// Delete
+//app.delete()
 
 
 // Escuchando......
