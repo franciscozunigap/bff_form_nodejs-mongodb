afd68530def4a6ff81ad255b3d459135afc5fca4
Francisco Zúñiga <francisco.zunigap@usm.cl>
2023-01-19 17:01:33 -0300

    19-01

--- app.js
+++ app.js
@@ -24,45 +24,22 @@ const {body, validationResult} = require("express-validator");
 const { response } = require("express");
 const userModel = mongoose.model("user", userSchema)
 
+// verificador
 
 const verify = async (req)=>{
     verificador = await userModel.findOne(req)
     return(verificador)
 }
 
+
+
+// view
+
 const mostrar = async ()=>{
     
     const usuarios = await userModel.find()
     
     return (usuarios)
-
-}
-
-//actualizar
-
-const actualizar = async(id, new_data)=>{
-
-    const user_act = await userModel.updateOne({
-        
-        id: id
-    
-        }, 
-        {
-        
-        $set: new_data
-
-        })
-
-    return true
-
-}
-
-//delete
-
-const eliminar = async(id_delete)=>{
-    const for_delete = await userModel.deleteOne({id: id_delete})
-    
-    return true
 
 }
 
@@ -104,8 +81,7 @@ app.post("/ingresar", [
         if(val!==null){
             return res.json({code: 200});
             
-            
-
+        
         }else{
             return res.json({code: 201});
             
@@ -113,7 +89,13 @@ app.post("/ingresar", [
             
         })
     })
+/*/
 
+    //usuario a crear está en req.body (no posee id)
+    req.body.id = uuid(); // se le asigna id
+    //userModel.create(req.body); //crea usuario
+    return res.json({code: 301})
+    /*/
 
 
 //REGISTER
@@ -131,12 +113,14 @@ app.post("/register", [
     
     verify(req.body).then((val) => {
     if(val!==null){
+        
         return res.json({code: 300}); ////este usuario ya existe
 
     }else{
         //usuario a crear está en req.body (no posee id)
-        req.body.id = uuid(); // se le asigna id
-        userModel.create(req.body); //crea usuario
+        new_body = {user: req.body.user, password: req.body.password, id: uuid()}
+
+        userModel.create(new_body); //crea usuario
         return res.json({code: 301})
 
         
@@ -145,24 +129,45 @@ app.post("/register", [
 )
 })
 
-/*/
 // Update
+app.post('/update-user', function(req, res) {
+    let body = req.body;
 
-app.post("/update", [],(req, res)=>{
-    actualizar(req.body.id, req.body).then((val)=>{
-        res.send("OK ACTUALIZADO")
+    userModel.updateOne({ id: body.id }, {
+            $set: {
+                user: body.user,
+                password: body.password,
+            }
+        },
+        function(error, info) {
+            if (error) {
+                res.json({code: 400}); //no actualizado
 
-    })
+            } else {
+               res.json({code: 204}) // actualizado
+            }
+        }
+    )
+});
 
     
-    
-}
-)
 
-/*/
 
 // Delete
-//app.delete()
+app.post('/delete-user', function(req, res){
+    let body = req.body;
+    userModel.deleteOne({id: body.id},
+    function(error, info) {
+            if (error) {
+                res.json({code: 400}) // no eliminado
+            } else {
+                res.json({code: 200}) // eliminado
+            }
+     
+
+})
+
+})
 
 
 // Escuchando......
