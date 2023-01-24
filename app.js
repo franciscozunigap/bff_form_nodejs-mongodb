edfbe14fe87bbe58368dd75840060bc2921e0322
Francisco Zúñiga <francisco.zunigap@usm.cl>
2023-01-24 17:25:36 -0300

    24-01 comenzando a encriptar

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const mongoose = require("mongoose");
+const crypto = require("crypto");
 const {v4: uuid} = require("uuid");
-
-
+const {body, validationResult} = require("express-validator");
+const { response } = require("express");
 const app = express()
 const url = "mongodb://127.0.0.1:27017/form" 
 
@@ -21,9 +22,20 @@ const userSchema = mongoose.Schema({
 })
 
 
-const {body, validationResult} = require("express-validator");
-const { response } = require("express");
-const userModel = mongoose.model("user", userSchema)
+
+const userModel = mongoose.model("users", userSchema, "users")
+
+
+
+const sesionSchema = mongoose.Schema({
+    user: String,
+    id: String,
+    private_key: Object
+
+})
+
+const sesionModel = mongoose.model("sesion", sesionSchema, "sesion")
+
 
 // verificador
 
@@ -42,6 +54,8 @@ const mostrar = async ()=>{
     return (usuarios)
 
 }
+
+
 
 
 
@@ -67,7 +81,6 @@ app.get("/admin",(req, res)=>{
     
     
 });
-
 
 
 //LOGIN
@@ -128,7 +141,7 @@ app.post("/register", [
         //usuario a crear está en req.body (no posee id)
         new_body = {user: req.body.user, password: req.body.password, id: uuid(), type: req.body.type}
         userModel.create(new_body); //crea usuario
-            return res.json({code: 301})
+        return res.json({code: 301})
             
 
         }
@@ -145,10 +158,17 @@ app.post('/update-user', function(req, res) {
     let body = req.body;
 
     userModel.updateOne({ id: body.id }, {
-            $set: {
+            
+             $set: 
+             
+             {
                 user: body.user,
                 password: body.password,
             }
+
+
+
+            
         },
         function(error, info) {
             if (error) {
@@ -179,6 +199,72 @@ app.post('/delete-user', function(req, res){
 })
 
 })
+
+
+
+// usuario ingresado
+
+
+app.post("/usersesion", [
+    
+    body("user","Ingrese user correcto")
+        .exists(),
+        
+
+],(req, res)=>{
+
+    verify(req.body).then((val) => 
+    {
+        if(val!==null){
+            // generar sesion
+
+            //console.log(val)
+            
+
+            const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
+                modulusLength: 2048,
+                });
+
+            //console.log(privateKey)
+    
+            // guardar en mongo (user, id, privatekey)
+            sesion = {
+                user: val.user,
+                id: val.id,
+                private_key: privateKey // 
+            }
+
+            
+            console.log(sesion)
+
+            sesionModel.create(sesion)
+
+            msj = {
+                user: val.user,
+                id: val.id,
+                public_key: publicKey // 
+            }
+
+
+            return res.json(msj)
+            //return res.json({code: 200});
+         
+        
+        }else{
+            return res.json({code: 201});
+            
+        }
+            
+        })
+
+
+
+
+
+    
+    })
+
+
 
 
 // Escuchando......
